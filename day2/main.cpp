979bad9ab23b37d009523d01477371bf55499e2a
Ionuț Leonte <ionut.leonte@gmail.com>
2020-12-02 09:50:51 +0200

    day2: complete

--- day2/main.cpp
+++ day2/main.cpp
@@ -1,3 +1,97 @@
+#include <aoc.h>
+
+struct Password {
+    std::array<size_t, 256> chars{};
+
+    static inline Password parse(std::string_view sv) {
+        while (!sv.empty() && std::isspace(*sv.begin()))
+            sv.remove_prefix(1);
+
+        Password ret{};
+        std::fill(ret.chars.begin(), ret.chars.end(), 0);
+        for (auto ch : sv)
+            ret.chars[ch] += 1;
+        return ret;
+    }
+};
+std::ostream& operator<<(std::ostream& out, const Password& p) {
+    out << '{';
+    bool first_print = true;
+    for (size_t ch = 32; ch < 128; ch++) {
+        if (!p.chars[ch]) continue;
+        if (first_print)
+            first_print = false;
+        else
+            out << ',' << ' ';
+        out << static_cast<char>(ch) << ':' << ' ' << p.chars[ch];
+    }
+    out << '}';
+    return out;
+}
+
+struct Policy {
+    size_t min = 0;
+    size_t max = 0;
+    size_t ch = 0;
+
+    static inline std::optional<Policy> parse(std::string_view sv) {
+        static std::regex policy_re(
+            "^\\s*(\\d+)-(\\d+)\\s+(.)\\s*$",
+            std::regex_constants::ECMAScript | std::regex_constants::optimize);
+
+        std::cmatch match{};
+        if (!std::regex_match(sv.begin(), sv.end(), match, policy_re))
+           return std::nullopt;
+
+        Policy ret{};
+        std::from_chars(match[1].first, match[1].second, ret.min);
+        std::from_chars(match[2].first, match[2].second, ret.max);
+        ret.ch = static_cast<size_t>(*match[3].first);
+
+        return ret;
+    }
+
+    inline size_t matches_p1(const Password& passwd) const {
+        return passwd.chars[ch] == std::clamp(passwd.chars[ch], min, max);
+    }
+
+    inline size_t matches_p2(std::string_view passwd) const {
+        auto ch1 = min <= passwd.size() ? static_cast<size_t>(passwd[min - 1]) : 0;
+        auto ch2 = max <= passwd.size() ? static_cast<size_t>(passwd[max - 1]) : 0;
+        return ((ch == ch1) && (ch != ch2)) || ((ch != ch1) && (ch == ch2));
+    }
+};
+std::ostream& operator<<(std::ostream& out, const Policy& p) {
+    return out << '{' << p.min << "-" << p.max << " " << static_cast<char>(p.ch) << '}';
+}
+
 int main() {
+    static std::regex line_re(
+        "^(.+):\\s*(.+)$",
+        std::regex_constants::ECMAScript | std::regex_constants::optimize);
+
+    size_t count_p1 = 0;
+    size_t count_p2 = 0;
+
+    std::string line{};
+    while (std::getline(std::cin, line)) {
+        if (line.empty()) continue;
+
+        std::cmatch match{};
+        if (!std::regex_match(&*line.cbegin(), &*line.cend(), match, line_re))
+            continue;
+
+        if (auto r = Policy::parse(std::string_view(match[1].first, match[1].length())); r.has_value()) {
+            auto passwd_sv = std::string_view(match[2].first, match[2].length());
+            auto policy = r.value();
+            auto passwd = Password::parse(passwd_sv);
+            count_p1 += policy.matches_p1(passwd);
+            count_p2 += policy.matches_p2(passwd_sv);
+        }
+    }
+
+    fmt::print("{}\n", count_p1);
+    fmt::print("{}\n", count_p2);
+
     return 0;
 }
